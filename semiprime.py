a941fb9c6b686d1c02f5b103f8d927442b3d29bc
Luca <lucatolomei@icloud.com>
2024-02-28 11:04:07 +0100

    completato algoritmo in radice di n

--- semiprime.py
+++ semiprime.py
@@ -1,7 +1,9 @@
 from math import sqrt
 
 def es(n):
-    bound = int(sqrt(n))
+    bound = int(sqrt(n))+1
+    count = 0
     for i in range(2, bound):
         if n % i == 0:
-            temp = n // i
+            count += 1
+    return count == 1
