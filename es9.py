1842a0b94b921313fa4b6c42caaadbb298a3c753
ToloCops <tolomei.1945177@studenti.uniroma1.it>
2024-03-05 17:13:46 +0100

    risolto es9

---
+++ es9.py
@@ -0,0 +1,26 @@
+def conta_pozzi_da(u: int, G: list[list[int]], V: list[int]) -> int:
+    V[u] = 1
+    if G[u] == []: return 1
+    count = 0
+    for x in G[u]:
+        if V[x] == 0:
+            count += conta_pozzi_da(x, G, V)
+    return count
+
+G: list[list[int]] = [
+    [9, 2],
+    [0, 2, 5, 7],
+    [],
+    [4, 6],
+    [10],
+    [],
+    [9],
+    [8],
+    [],
+    [10],
+    [3]
+]
+
+u = 1
+V = [0] * len(G)
+print(conta_pozzi_da(u, G, V))
