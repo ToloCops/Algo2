51eea89b1a40c78bbaeae2b58fcff13bec9a4d7f
ToloCops <tolomei.1945177@studenti.uniroma1.it>
2024-03-05 13:00:26 +0100

    codice ripulito

--- es8.py
+++ es8.py
@@ -6,18 +6,16 @@ def complete_graph_gen(n: int) -> list[list[int]]:
     return G
 
 def edge_orienting(u: int, G: list[list[int]], V: list[int]) -> list[list[int]]:
-    print(u)
     V[u] = 1
     for x in reversed(G[u]):
         if V[x] == 0:
             G[x].pop()
     if u == 0: return G
-    else: 
-        edge_orienting(u-1, G, V)
-        return G
+    return edge_orienting(u-1, G, V)
 
 
-n = 4
+n = 3
 G = complete_graph_gen(n)
 V = [0] * n
+print(G)
 print(edge_orienting(n-1, G, V))
