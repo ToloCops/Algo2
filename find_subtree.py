9734084e940c758eab58d0929c462e9ffdd0cc53
ToloCops <tolomei.1945177@studenti.uniroma1.it>
2024-03-12 17:15:17 +0100

    finita funzione subtree

--- find_subtree.py
+++ find_subtree.py
@@ -1,5 +1,32 @@
+def path_to_root(P: list[int], u: int, x: int) -> tuple[list[int], bool]:
+    path = []
+    is_sub = False
+    path.append(u)
+    if u == x: 
+        is_sub = True
+        return path, is_sub
+    while P[u] != u:
+        if P[u] == -1: 
+            is_sub = False
+            break
+        path.append(P[u])
+        u = P[u]
+        if u == x: 
+            is_sub = True
+            break
+    return path, is_sub
+
 def subtree(P: list[int], x: int) -> list[int]:
-    return [0]
+    subtree = []
+    for i in range(len(P)):
+        if P[i] != -1:
+            path, is_sub = path_to_root(P, i, x)
+            if not is_sub:
+                for u in path:
+                    P[u] = -1
+    for i in range(len(P)):
+        if P[i] != -1: subtree.append(i+1)
+    return subtree
 
 P = [2, 3, 2, 4, 2, 3, 4, 0]
 x = 4
