ea6cdb26d326ee16a56a5820621ddcb0a7f628b5
ToloCops <tolomei.1945177@studenti.uniroma1.it>
2024-03-13 18:36:12 +0100

    risolta funzione di classificazione archi

--- arch_finder.py
+++ arch_finder.py
@@ -1,16 +1,23 @@
-def DFSr_directed(u: int, G: list[list[int]], V: list[int]) -> bool:
+def DFSr(u: int, G: list[list[int]], V: list[int], D: list[int], archs: list[int], time: list[int] = [0]):
     V[u] = 1
+    D[u] = time[0]
+    time[0] += 1
     for v in G[u]:
-        if V[u] == 1: return True
-        if V[u] == 0:
-            if DFSr_directed(v, G, V): return True
+        if V[v] == 1: archs[1] += 1
+        elif V[v] == 0:
+            DFSr(v, G, V, D, archs, time)
+        elif V[v] == 2:
+            if D[u] < D[v] : archs[0] += 1
+            else: archs[2] += 1
     V[u] = 2
-    return False
 
 
-def find_archs(G: list[list[int]]) -> tuple[int, int, int]:
-    
-    return 0, 0, 0
+def find_archs(G: list[list[int]]) -> list[int]:
+    archs = [0, 0, 0]
+    V = [0] * len(G)
+    D = [0] * len(G) #lista contenente il momento in cui i nodi sono stati scoperti
+    DFSr(0, G, V, D, archs)
+    return archs
 
 
 
