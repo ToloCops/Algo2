dc67369cd70b9c2f2e46fa2b8564cdf16ec63431
ToloCops <tolomei.1945177@studenti.uniroma1.it>
2024-03-13 11:02:10 +0100

    minor fix

--- arch_finder.py
+++ arch_finder.py
@@ -1,5 +1,18 @@
+def DFSr_directed(u: int, G: list[list[int]], V: list[int]) -> bool:
+    V[u] = 1
+    for v in G[u]:
+        if V[u] == 1: return True
+        if V[u] == 0:
+            if DFSr_directed(v, G, V): return True
+    V[u] = 2
+    return False
+
+
 def find_archs(G: list[list[int]]) -> tuple[int, int, int]:
+    
     return 0, 0, 0
+
+
 
 G = [[1, 2], [3], [3], [4, 5], [5], [6], [1]]
 print(find_archs(G))
