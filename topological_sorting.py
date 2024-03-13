c22d3c7df6bb0c516c6add944ec91f4754f94e20
ToloCops <tolomei.1945177@studenti.uniroma1.it>
2024-03-13 09:45:36 +0100

    aggiunto algoritmo per sort topologico

---
+++ topological_sorting.py
@@ -0,0 +1,18 @@
+def DFSr(G: list[list[int]], x: int, V: list[int], sorted: list[int]):
+    V[x] = 1
+    for y in G[x]:
+        if V[y] == 0:
+            DFSr(G, y, V, sorted)
+    sorted.append(x)
+
+def topo_sort(G: list[list[int]]) -> list[int]:
+    V = [0 for _ in G]
+    sorted = []
+    for x in range(len(G)):
+        if V[x] == 0:
+            DFSr(G, x, V, sorted)
+    sorted.reverse()
+    return sorted
+
+G = [[1, 4, 5], [2], [3], [4], [], [2], [2]]
+print(topo_sort(G))
