f1d0857e8e14c21aecee7b476afe4a1893df7b27
ToloCops <tolomei.1945177@studenti.uniroma1.it>
2024-03-13 11:08:15 +0100

    risolti problemi di tipo in topological_sorting.py

--- topological_sorting.py
+++ topological_sorting.py
@@ -7,12 +7,12 @@ def DFSr(G: list[list[int]], x: int, V: list[int], sorted: list[int]):
 
 def topo_sort(G: list[list[int]]) -> list[int]:
     V = [0 for _ in G]
-    sorted = []
+    sorted: list[int] = []
     for x in range(len(G)):
         if V[x] == 0:
             DFSr(G, x, V, sorted)
     sorted.reverse()
     return sorted
 
-G = [[1, 4, 5], [2], [3], [4], [], [2], [2]]
+G: list[list[int]] = [[1, 4, 5], [2], [3], [4], [], [2], [2]]
 print(topo_sort(G))
